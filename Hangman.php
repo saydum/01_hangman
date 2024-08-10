22a24a6f97a5e378f545ab2cad2fe6e3452ddc7e
Saydum <saydum.it5@gmail.com>
2024-08-10 18:08:48 +0300

    feat: реализован вывод ui элементов по шагам
    - получение ввода от пользователя

--- Hangman.php
+++ Hangman.php
@@ -1,5 +1,4 @@
 <?php
-$words = ['apple', 'internet', 'football', 'moscow'];
 
 /**
  * 1) Установить слово из словаря
@@ -8,6 +7,20 @@ $words = ['apple', 'internet', 'football', 'moscow'];
  * 4) Полученную букву проверить если да то вывести и снова попросить ввод пока попытки не закончатся
  * 5) Если попытка закончится ожидаем две команды 0-close 1-restart
  */
+
+// Словарь
+$words = include './words.php';
+
+// Кол-во попыток
+$count = 6;
+
+// UI Элементы
+$ui = [
+    'O ', // 0
+    '|',  // 1
+    '/',  // 2
+    "\\", // 3
+];
 
 
 function setRandWord(array $words): string
@@ -32,4 +45,52 @@ function hiddenAndShowLetter(string $word, string $letter): string
     return implode('', $stateWord);
 }
 
+function showUiElementsBySteps(
+    string $step1 = ' ',
+    string $step2 = ' ',
+    string $step3 = ' ',
+    string $step4 = ' ',
+    string $step5 = ' ',
+    string $step6 = ' '
+): void
+{
+    $format = "
+        ----------
+           |     |
+           %s    |
+          %s%s%s    |
+          %s %s    |
+                 |
+    --------------
+    ";
+
+    echo sprintf(
+        $format,
+        $step1,
+        $step2,
+        $step3,
+        $step4,
+        $step5,
+        $step6,
+    );
+}
+
+
+// По шагово с условием
+//$step1 = $ui[0];
+//$step2 = $ui[2];
+//$step3 = $ui[1];
+//$step4 = $ui[3];
+//$step5 = $ui[2];
+//$step6 = $ui[3];
+//showUiElementsBySteps($step1, $step2, $step3, $step4, $step5, $step6);
+
+function inputLetterUser(): string
+{
+    $input = readline();
+
+    if (strlen($input) > 1) return 'error: max letter 1.';
+
+    return $input;
+}
 
---
+++ words.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'apple',
+    'internet',
+    'football',
+    'moscow'
+];
